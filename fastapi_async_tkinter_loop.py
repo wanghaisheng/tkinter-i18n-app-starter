c861acab7da4c6d545448fe0f14d870d3b22ade2
HeisenBerg? <admin@tiktokastudio.com>
2023-12-12 16:14:31 +0800

    Update fastapi_async_tkinter_loop.py

--- fastapi_async_tkinter_loop.py
+++ fastapi_async_tkinter_loop.py
@@ -14,12 +14,15 @@ from typing import Optional
 import platform
 from batchrunworker import BatchWorker
 from string import ascii_uppercase
-
+import time,os
+import psutil
+import threading
 uvicorn_subprocess: Optional[Process] = None
-
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+path=ROOT_DIR+os.sep+'assets/icon.ico'
 
 console_encoding = "utf-8"
-
+process_name=None
 if platform.system() == "Windows":
     from ctypes import windll
 
@@ -45,8 +48,8 @@ def start(lang, root=None):
     global mainwindow, canvas
 
     # root.resizable(width=True, height=True)
-    root.iconbitmap("assets/icon.ico")
-    root.title('tkinter asyncio demo')
+    root.iconbitmap(path)
+    root.title('tkinter async_tkinter_loop demo')
     Button(master=root, text="async_handler Asyncio Tasks", command=async_handler(do_tasks)).pack()
 
 
@@ -63,8 +66,31 @@ def quit_window(icon):
     icon.stop()
 
     print('Shutdown server')
-    if uvicorn_subprocess is not None:
-        uvicorn_subprocess.kill()
+    try:
+        print('server is  there')
+    # We need to make sure we won't kill different process
+    # https://stackoverflow.com/questions/71814738/asyncio-terminate-subprocess-on-wait-for-timeout
+    # https://python.readthedocs.io/en/stable/library/asyncio-subprocess.html#asyncio.asyncio.subprocess.Process
+    # returncode
+    # Return code of the process when it exited. A None value indicates that the process has not terminated yet.
+
+    # A negative value -N indicates that the child was terminated by signal N (Unix only).
+        if uvicorn_subprocess.returncode is None:
+            parent = psutil.Process(uvicorn_subprocess.pid)
+            for child in parent.children(recursive=True): 
+                child.terminate()
+            parent.terminate()
+        # uvicorn_subprocess.kill()
+        print('server is shutdown now')
+
+    except:
+        print('server is shutdown already')
+    if uvicorn_subprocess.returncode is not None:
+        print('check result server is there ')
+
+    else:
+        print('check result server is shutdown already')
+
     print('Shutdown root')
     # https://github.com/insolor/async-tkinter-loop/issues/10
     root.quit()
@@ -80,7 +106,7 @@ def show_window(icon, item):
 
 def withdraw_window():
     root.withdraw()
-    image = Image.open("assets/icon.ico")
+    image = Image.open(path)
 
     menu = (item("Quit", lambda icon:quit_window(icon)),
             item("Show", show_window))
@@ -119,9 +145,36 @@ async def start_fastapi_server():
             item.cancel()
 
     uvicorn_subprocess = None
-def stop():
+
+async def is_running():
+    # https://stackoverflow.com/questions/65874648/python-asyncio-subprocess-how-to-see-whether-it-is-still-running
+    with suppress(asyncio.TimeoutError):
+        await asyncio.wait_for(uvicorn_subprocess.wait(), 1e-6)
+    return uvicorn_subprocess.returncode is None    
+def stop_notworking_demo():
+
+    print('Shutdown server')
+    # we should use uvicorn_subprocess.returncode
     if uvicorn_subprocess is not None:
-        uvicorn_subprocess.kill()
+
+
+        time.sleep(0.5)
+        uvicorn_subprocess.terminate() 
+
+    else:
+        print('server not started')
+
+def stop():
+#  this can not kill all the process and thread so you can not remove any exe files
+    print('Shutdown server')
+    if uvicorn_subprocess is not None:
+
+
+        time.sleep(0.5)
+        uvicorn_subprocess.terminate() 
+
+    else:
+        print('server not started')
 
 
 
